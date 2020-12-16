55e6b8e35a28cf0624f1ed05b4653dc7dde22d96
cep104 <candicelizabethpeters@gmail.com>
2020-12-16 15:44:27 -0600

    notes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -10,11 +10,23 @@ export default class ColorBox extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: this.props.opacity}}>
+        {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : null}
+        {/* take away .1 percent of opacity for each color box till the opacity is .1 then do nothing. we use 0.2 because once it goes through at 0.2 - 0.1 = 0.1 and will stop */}
       </div>
     )
   }
 
 }
+// another solution:
+// if(this.props.opacity >= 0.2) {
+//   return (
+//     <div className="color-box" style={{opacity: this.props.opacity}}>
+//       <ColorBox opacity={this.props.opacity - 0.1} />
+//     </div>
+//   )
+// } else {
+//   return null
+// }
+// }
 
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,17 @@
 //your code here
+import React, { Component } from 'react'
+
+
+export default class Comment extends Component {
+  render() {
+    return (
+      <div className="comment">
+        {this.props.commentText}
+      </div>
+    )
+  }
+}
+// 1) BlogPost
+//        "before all" hook:
+//      TypeError: Enzyme::Selector does not support an array, null, or empty object as a selector
+//this error means it needs to have the comment exported and import react!
